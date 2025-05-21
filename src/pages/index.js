c79e68783901a6f3ca5089bd064d83b78436b1f6
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-21 11:50:31 -0400

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './home';
+import Error from './error';
+import About from './about-me';
+import Project from './projects';
+import Contact from './contact-me';
+export {Home, Error, About, Project, Contact};
